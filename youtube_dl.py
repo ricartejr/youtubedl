eb8f9cf089aeeb784abde66f1794bb594dd66032
Ricarte Jr <ricarte.junior@gmail.com>
2022-10-19 20:56:20 -0300

    Modificando nombres de variables y errores

--- youtube_dl.py
+++ youtube_dl.py
@@ -3,43 +3,43 @@ from pytube import Playlist, YouTube
 from funciones.func import fix_jr
 from pytube.cli import on_progress
 
-contador = 0
-count_error = 0
+contador_videos = 0
+contador_error = 0
 lista_de_errores = ['']
 c_padrao = r'D:/VIDEOS/Videos YT/1 - Python Downloads'
 url_yt = Playlist(input('YOUTUBE URL ->'))
 
 try:
     for video in url_yt.video_urls:
-        contador += 1
+        contador_videos += 1
         try:
             video = YouTube(video, on_progress_callback=on_progress)
             titulo_v = fix_jr(video.title)
             autor_v = video.author
-            autor_p = url_yt.owner
-            p_title = url_yt.title
-            pastas = f'{path.abspath(c_padrao + "/" + autor_p + "/" + fix_jr(p_title))}'
+            p_autor = url_yt.owner
+            p_titulo = url_yt.title
+            pastas = f'{path.abspath(c_padrao + "/" + p_autor + "/" + fix_jr(p_titulo))}'
             makedirs(pastas, exist_ok=True)
             video = video.streams.get_by_itag(22)
             new_name = f'{autor_v + " - " + titulo_v}.mp4'
-            print(f'-> {autor_v} - {p_title} - {titulo_v}')
+            print(f'-> {autor_v} - {p_titulo} - {titulo_v}')
             # video.download(output_path=pastas, filename=new_name)
         except KeyboardInterrupt:
-            print('\nCancelado pelo usuario\n')
+            print('\nCancelado por usuario\n')
             break
         except AttributeError:
-            count_error += 1
+            contador_error += 1
             lista_de_errores.append(f'{autor_v} - {titulo_v}')
         finally:
             print(f'\n{"":-<120}')
     else:
-        print(f'\n-> Download Total [ {contador} ]\n')
-        if count_error >= 1:
-            print(f'-> Errores Total [ {count_error} ]')
+        print(f'\n-> Download Total [ {contador_videos} ]\n')
+        if contador_error >= 1:
+            print(f'-> Errores Total [ {contador_error} ]')
             for nome in lista_de_errores:
                 print(nome)
             else:
-                print(f'\n')
+                print('\n')
 
 except KeyError:
-    print('ERROR')
+    print('-> ERROR - Playlist Invalida')
