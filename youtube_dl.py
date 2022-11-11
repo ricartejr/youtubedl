fcbcc4d1eb0ef86dcc4dc156a8b548f774b356eb
Ricarte Jr <ricarte.junior@gmail.com>
2022-11-11 20:58:01 -0300

    biblioteca rich

--- youtube_dl.py
+++ youtube_dl.py
@@ -2,16 +2,21 @@ from os import makedirs, path, system
 from pytube import Playlist, YouTube
 from pytube.cli import on_progress
 from funciones.func import fix_jr
-from rich import Console
+from rich.console import Console
+
 
 system('cls')
 contador_videos = 0
 contador_error = 0
 lista_de_errores = ['']
 c_padrao = r'D:/VIDEOS/Videos YT/1 - Python Downloads'
-url_yt = Playlist(input('YOUTUBE URL -> '))
+
+C = Console()
+C.rule('YOUTUBE - DOWNLOADS')
 
 try:
+    url_yt = Playlist(C.input('[r b] YOUTUBE URL [/] -> '))
+    print('')
     for video in url_yt.video_urls:
         contador_videos += 1
         try:
@@ -24,16 +29,19 @@ try:
             makedirs(pastas, exist_ok=True)
             video = video.streams.get_by_itag(22)
             new_name = f'{autor_v + " - " + titulo_v}.mp4'
-            print(f'-> {autor_v} - {p_titulo} - {titulo_v}')
-            #! video.download(output_path=pastas, filename=new_name)
+            C.print(
+                f'-> [r b] {autor_v} [/] - [bright_red on grey93 b] {p_titulo} [/] - [bright_green on grey93 b] {titulo_v} [/]')
+            # video.download(output_path=pastas, filename=new_name)
         except KeyboardInterrupt:
-            print('\nCancelado por usuario\n')
+            system('cls')
+            C.print('[r b] Cancelado por usuario [/]\n')
             break
         except AttributeError:
             contador_error += 1
             lista_de_errores.append(f'{autor_v} - {titulo_v}')
         finally:
-            print(f'\n{"":-<120}')
+            pass
+            # print(f'\n{"":-<120}')
     else:
         print(f'\n-> Download Total [ {contador_videos} ]\n')
         if contador_error >= 1:
@@ -44,6 +52,5 @@ try:
                 print('\n')
         else:
             print(f'-> Errores Total [ 0 ]\n\n')
-
 except KeyError:
     print('-> ERROR - Playlist Invalida')
